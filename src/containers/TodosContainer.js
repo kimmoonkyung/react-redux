4f06d9845c0fdf441c0e5c4dbb1460eab287413a
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-05-19 20:44:52 +0900

    21.05.19 리액트-리덕스 connect 마무리 ㅅㄱ 안쓸래!

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -24,3 +24,21 @@ export default connect((state) => ({ todos: state.todos }), {
     addTodo,
     toggleTodo,
 })(TodosContainer);
+
+// connect의 3번째 파라미터 mergeProps
+// 생략해도 상관 없는 파라미터,
+// 하나의 함수인데 이를 통해서 컴포넌트롤 통해 전달 받게 될 props 를 정의 할 수 있다
+/**
+    (stateProps, dispatchProps, ownProps) => Object
+    생략 된다면
+    { ...ownProps, ...stateProps, ...dispatchProps }
+    이런 형태의 객체를 리턴함
+ */
+
+// connect의 4번째 파라미터 options
+/**
+    {
+        ... 다양한 옵션들이 있음
+    }
+ */
+// 잘 사용하지 않아서 넘어가도록 한다.
